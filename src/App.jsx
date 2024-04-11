d64923947b7ec34d0dd2b9f648224a0a0c417f76
HannaPalianytsia <hannapalianytsia1@gmail.com>
2024-04-12 01:15:21 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import toast, { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
 import { getPhotos } from "./apiService/photos";
 import SearchBar from "./components/searchBar/SearchBar";
 import ImageGallery from "./components/imageGallery/ImageGallery";
